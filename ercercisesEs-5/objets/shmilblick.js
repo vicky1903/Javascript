6ad60f6495feec4bd699bd98aab38b8abe87ecd6
RAMIREZ-LASPRILLA Maria-Victoria <maria-victoria.m.ramirez-lasprilla@renault.com>
2020-06-16 14:06:10 +0200

    rangement

---
+++ ercercisesEs-5/objets/shmilblick.js
@@ -0,0 +1,19 @@
+var schmilblick = {
+    taille: "grand",
+    dimensions: {
+      largeur:2.5,
+      longueur:3,
+      hauteur:0.02
+    },
+    couleurs: [
+        "rouge",
+        "jaune",
+        "or",
+        "bleu"
+    ],
+    peutVoler: false,
+    matiere: "laine",
+    queSuisJe: function(){
+        alert("A votre avis que suis-je ?");
+    }
+};
